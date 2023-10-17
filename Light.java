37c32b68f26da1fb45d81d06972f3532a78d7dbd
Aidan Liam N. Battad <142377920+AidanBattad@users.noreply.github.com>
2023-10-17 21:45:50 +0800

    Create Light.java

---
+++ Light.java
@@ -0,0 +1,13 @@
+package commandPattern;
+
+class Light {
+    void turnOn()
+    {
+        System.out.println("Turning on the light.");
+    }
+
+    void turnOff()
+    {
+        System.out.println("Turning off the light.");
+    }
+}
